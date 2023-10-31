cfbb0fafd5bf446721e7d3688b7f70368a05ba59
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-10-31 09:25:44 +0700

    Add reproducer for aspect
    For me, the aspect is triggered just fine. There is no problem. The
    expected log line is written:
    
    c.p.errorhandling.aspect.ErrorAspect :
      Loki, it looks like it is not found in method
      getAccountById: Account with ID: 1 not found

--- bank-app/src/main/java/com/example/bankapp/BankAppApplication.java
+++ bank-app/src/main/java/com/example/bankapp/BankAppApplication.java
@@ -1,7 +1,9 @@
 package com.example.bankapp;
 
+import com.example.bankapp.service.AccountService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -12,7 +14,9 @@ public class BankAppApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(BankAppApplication.class, args);
+		try (ConfigurableApplicationContext context = SpringApplication.run(BankAppApplication.class, args)) {
+			context.getBean(AccountService.class).getAccountById("1");
+		}
 	}
 
 }
